e0bafbc2697b9e98a9c87ecdb74e3fc829286a5f
MOULIDHARREDDY <moulidharreddy2006@gmail.com>
2026-02-07 05:55:10 -0800

    Add files via upload

---
+++ LAB_04/output_lab04.py
@@ -0,0 +1,17 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+======== RESTART: D:\Sem4\KBDVM\MOULI\ML\BL.SC.U4AIE24143_LAB04\lab4.py ========
+TRAINING RESULTS
+[[1076   10    2]
+ [   4 1121    0]
+ [   0    0 1144]]
+0.9952418561720716 0.9952338397378612 0.9952308519775154
+TESTING RESULTS
+[[264  15   3]
+ [  6 255   0]
+ [  0   0 297]]
+0.9717420634920635 0.9714285714285714 0.9713388502677616
+Not enough numeric features to plot scatter graph.
+Best k: {'n_neighbors': 1}
+Best CV Score: 0.9946388652331276
